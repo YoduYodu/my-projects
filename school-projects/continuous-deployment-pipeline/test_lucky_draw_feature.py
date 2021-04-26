1ed756f0113781278c25abe44e9a15bdb0aedb0c
yoduyodu <wang7ong@gmail.com>
2021-04-26 13:06:38 +0800

    Fix bug

--- school-projects/continuous-deployment-pipeline/test_lucky_draw_feature.py
+++ school-projects/continuous-deployment-pipeline/test_lucky_draw_feature.py
@@ -10,19 +10,19 @@ scenarios('./features/app.feature', example_converters=dict(number=int, result=s
 
 @pytest.fixture
 @when('the Lucky Draw API is queried with <number>')
-def rm_response(number):
+def luck_draw_response(number):
     response = requests.get(API + str(id))
     return response
 
 
 @then('the response status code is 200')
-def rm_response_code(rm_response):
-    assert rm_response.status_code == 200
+def luck_draw_response_code(luck_draw_response):
+    assert luck_draw_response.status_code == 200
 
 
-@then('the response shows <name>')
-def rm_response_name(rm_response, name):
-    assert name == rm_response.json()['name']
+@then('the response shows <result>')
+def luck_draw_response_result(luck_draw_response, result):
+    assert result == luck_draw_response
 
 
 
